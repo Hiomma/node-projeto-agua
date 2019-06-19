74e913b0158ca8ea22f991fcbe016c456d321d63
Gabriel Sereno <gabrielf2@live.com>
2019-06-19 13:36:05 -0300

    Mandando pro note

--- controllers/imagem.controller.js
+++ controllers/imagem.controller.js
@@ -8,6 +8,7 @@ var router = express.Router();
 function imagemController() {
 
     router.route("/imagem/:id").post(uploadImagem);
+    router.route("/slide/imagem/:id").post(uploadSlideImagem);
 
     return router;
 
@@ -20,6 +21,32 @@ function imagemController() {
             })
 
             await models.Imagem.destroy({ where: { noticia_id: req.params.id } });
+
+            for (let aux of req.files) {
+                await models.Imagem.build({
+                    url: aux.path.replace(/\\/g, "/"),
+                    noticia_id: req.params.id
+                }).save();
+            }
+
+            return res.send(req.files);
+        }
+
+
+
+        return res.status(500).send('Houve erro no upload!');
+    }
+    ASDKJASKLDJLAKSDJ TO TERMINANDO A IMAGEM DO SLIDE
+
+    async function uploadSlideImagem(req, res) {
+        if (req.files.length > 0) {
+            await models.Slide.findAll({ raw: true, where: { noticia_id: req.params.id } }).then(data => {
+                for(let aux of data){
+                    fs.unlink(aux.url, () => {})
+                }
+            })
+
+            await models.Slide.destroy({ where: { noticia_id: req.params.id } });
 
             for (let aux of req.files) {
                 await models.Imagem.build({
