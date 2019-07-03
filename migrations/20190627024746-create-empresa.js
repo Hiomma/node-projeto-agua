63babaf2ba771c8ce82699715352c04afd2cb2ff
Gabriel Sereno <gabrielf2@live.com>
2019-07-02 23:12:37 -0300

    Adicionar migrations

--- migrations/20190627024746-create-empresa.js
+++ migrations/20190627024746-create-empresa.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Empresas', {
+    return queryInterface.createTable('empresas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Empresas');
+    return queryInterface.dropTable('empresas');
   }
 };
