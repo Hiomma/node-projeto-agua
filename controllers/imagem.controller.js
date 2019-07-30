7ba071dbc19f31c5d12ccf0d46eb1d0b88e8a749
Gabriel Sereno <gabrielf2@live.com>
2019-07-30 02:26:38 -0300

    Terminado o site

--- controllers/imagem.controller.js
+++ controllers/imagem.controller.js
@@ -8,10 +8,15 @@ var router = express.Router();
 function imagemController() {
 
     router.route("/imagem/:id").post(uploadImagem);
-    router.route("/slide/imagem/:id").post(uploadSlideImagem);
+    router.route("/imagem/principal/:id").post(uploadImagemPrincipalNoticia);
+    router.route("/parceiro/imagem/:id").post(uploadParceiro);
 
     return router;
 
+
+    /**
+     * @description Upload das fotos da noticia
+     */
     async function uploadImagem(req, res) {
         if (req.files.length > 0) {
             await models.Imagem.findAll({ raw: true, where: { noticia_id: req.params.id } }).then(data => {
@@ -35,21 +40,49 @@ function imagemController() {
         return res.status(500).send('Houve erro no upload!');
     }
 
-    async function uploadSlideImagem(req, res) {
+    /**
+     * @description Upload da foto principal da noticia
+     */
+    async function uploadImagemPrincipalNoticia(req, res) {
         if (req.files.length > 0) {
-            await models.Slide.findAll({ raw: true, where: { noticia_id: req.params.id } }).then(data => {
-                for (let aux of data) {
-                    fs.unlink(aux.url, () => { })
-                }
+            await models.Noticia.findByPk(req.params.id).then(data => {
+                fs.unlink(data.imagem, () => { })
             })
 
-            await models.Slide.destroy({ where: { noticia_id: req.params.id } });
+            for (let aux of req.files) {
+                await models.Noticia
+                    .findByPk(req.params.id)
+                    .then((noticia) => {
+
+                        noticia.imagem = aux.path.replace(/\\/g, "/");
+                        return noticia.update({ imagem: noticia.imagem });
+                    });
+            }
+
+            return res.send(req.files);
+        }
+
+        return res.status(500).send('Houve erro no upload!');
+    }
+
+    /**
+    * @description Upload da foto da tabela da parceiro
+    */
+    async function uploadParceiro(req, res) {
+        if (req.files.length > 0) {
+            await models.Parceiro.findByPk(req.params.id).then(data => {
+                if (data)
+                    fs.unlink(data.url, () => { })
+            })
 
             for (let aux of req.files) {
-                await models.Imagem.build({
-                    url: aux.path.replace(/\\/g, "/"),
-                    noticia_id: req.params.id
-                }).save();
+                await models.Parceiro
+                    .findByPk(req.params.id)
+                    .then((parceiro) => {
+
+                        parceiro.url = aux.path.replace(/\\/g, "/");
+                        return parceiro.update({ url: parceiro.url });
+                    });
             }
 
             return res.send(req.files);
